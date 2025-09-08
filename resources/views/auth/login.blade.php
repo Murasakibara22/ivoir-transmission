427c48260b68cb3b19cf1506d2530c7b9d1e4d07
Joackim Coulibaly <joackimcoulibaly5@gmail.com>
2025-09-08 11:21:21 +0000

    change asset to secure assets to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,18 +11,18 @@
     <meta content="Tableau de bord IVOIRE TRANSMISSION" name="description" />
     <meta content="joackim_clby" name="author" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ asset('icologIVOIRE TRANSMISSION.jpg')}}">
+    <link rel="shortcut icon" href="{{secure_asset('icologIVOIRE TRANSMISSION.jpg')}}">
 
     <!-- Layout config Js -->
-    <script src="{{ asset('assets/js/layout.js') }}"></script>
+    <script src="{{secure_asset('assets/js/layout.js') }}"></script>
     <!-- Bootstrap Css -->
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{secure_asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- Icons Css -->
-    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{secure_asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- App Css-->
-    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{secure_asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- custom Css-->
-    <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{secure_asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
 
     @stack('styles')
 
@@ -41,15 +41,15 @@
     <!-- end auth-page-wrapper -->
 
     <!-- JAVASCRIPT -->
-    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
-    <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
-    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+    <script src="{{secure_asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{secure_asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{secure_asset('assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{secure_asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+    <script src="{{secure_asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+    <script src="{{secure_asset('assets/js/plugins.js') }}"></script>
 
     <!-- password-addon init -->
-    <script src="{{ asset('assets/js/pages/password-addon.init.js') }}"></script>
+    <script src="{{secure_asset('assets/js/pages/password-addon.init.js') }}"></script>
 </body>
 
 

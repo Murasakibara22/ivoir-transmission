27973dbbfbac4705707c2b2262cb4ab42b63439c
Joackim Coulibaly <joackimcoulibaly5@gmail.com>
2025-10-31 15:41:03 +0000

    fix-navbar: modification navbar

--- resources/views/Entreprise/partials/navbar.blade.php
+++ resources/views/Entreprise/partials/navbar.blade.php
@@ -165,15 +165,12 @@
                 </div>
 
                 <div class="p-2 border-t border-slate-700">
-                    <form method="POST" action="{{ route('deconnexion') }}">
-                        @csrf
-                        <button type="submit" class="dropdown-item danger w-full text-left">
+                        <a href="{{ route('deconnexion') }}" type="button" class="dropdown-item danger w-full text-left">
                             <svg class="w-4 h-4 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
                             </svg>
                             Se déconnecter
-                        </button>
-                    </form>
+                        </a>
                 </div>
             </div>
         </div>
